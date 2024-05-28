ecec15e161086209768707ea978702dddc50cb72
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-05-28 09:53:01 +0900

    TypeScript 프로젝트의 양식 제출

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos=[
+    new Todo('Learn React'),
+    new Todo('Learn TypeScript')
+  ]
   return (
     <div>
-      <Todos items={['Learn React','Learn Type']}/>
+      <Todos items={todos}/>
     </div>
   );
 }
